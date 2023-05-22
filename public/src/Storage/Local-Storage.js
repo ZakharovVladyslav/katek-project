ce48dad56174298fc2e3191e330677518b3f90a3
ZakharovVladyslav <vladislavozakharov@gmail.com>
2023-05-22 19:11:05 +0300

    Deleted secondary storage for not being needed

--- public/src/Storage/Local-Storage.js
+++ public/src/Storage/Local-Storage.js
@@ -1,32 +1,10 @@
-export class CustomStorage {
+export default class CustomStorage {
     constructor() {
         if (!CustomStorage._core)
             CustomStorage._core = {};
 
         this._core = CustomStorage._core;
         window.CustomStorage = this;
-    }
-
-    get items() {
-        return this._core;
-    }
-
-    setItem(prop, value) {
-        this._core[prop] = value;
-    }
-
-    clearStorage() {
-        this._core = {};
-    }
-}
-
-export class SecondaryStorage {
-    constructor() {
-        if (!SecondaryStorage._core)
-            SecondaryStorage._core = {}
-
-        this._core = SecondaryStorage._core;
-        window.SecondaryStorage = this;
     }
 
     get items() {
