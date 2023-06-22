1d0ef33b3c5ae1a99b32f4cdfb7b11c08b85ce93
ZakharovVladyslav <vladislavozakharov@gmail.com>
2023-06-23 00:07:18 +0300

    Added docker

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
+import chalk from 'chalk';
 
 const app = express();
 const PORT = 3000;
@@ -17,39 +18,40 @@ const connection = mysql.createConnection({
 connection.connect();
 
 app.get('/:action', (req: Request, res: Response) => {
+	console.log(chalk.red('_____________________________________________________________________________________________'))
+	console.log(chalk.green(req.originalUrl));
+
 	let dateQuery = '';
 	let query = '';
 	const keysToAvoid = ['firstDate', 'secondDate', 'dateOption'];
-
-	console.log(req.query);
 
 	const sqlQueryParams = Object.entries(req.query).map(([key, value]) => {
 		if (!keysToAvoid.includes(key))
 			return `${key}='${value}'`;
 	}).filter(param => param !== undefined);
 
-	if (req.query.firstDate && req.query.secondDate)
-		dateQuery += `${req.query.dateOption}
-        BETWEEN STR_TO_DATE('${req.query.firstDate}', '%Y-%m-%d %H:%i.%s.%f')
-        AND STR_TO_DATE('${req.query.secondDate}', '%Y-%m-%d %H:%i.%s.%f')`;
+	if (req.query.firstDate && req.query.secondDate) {
+		dateQuery += `${req.query.dateOption} BETWEEN STR_TO_DATE('${req.query.firstDate}', '%Y-%m-%d %H:%i.%s.%f') AND STR_TO_DATE('${req.query.secondDate}', '%Y-%m-%d %H:%i.%s.%f')`;
+	}
 
 	if (sqlQueryParams.length === 0 && dateQuery !== '') {
-		console.log('case: sqlQueryParams.length === 0 && dateQuery !== ""');
+		console.log(chalk.red('\ncase: sqlQueryParams.length === 0 && dateQuery !== ""'));
 		query = ` WHERE ${dateQuery}`;
 	}
 	else if (sqlQueryParams.length !== 0 && dateQuery === '') {
-		console.log('case: sqlQueryParams.length !== 0 && dateQuery === ""');
+		console.log(chalk.red('\ncase: sqlQueryParams.length !== 0 && dateQuery === ""'));
 		query = ` WHERE ${sqlQueryParams.join(' AND ')}`;
 	}
 	else if (sqlQueryParams.length !== 0 && dateQuery !== '') {
-		console.log('case: sqlQueryParams.length !== 0 && dateQuery !== ""');
+		console.log(chalk.red('\ncase: sqlQueryParams.length !== 0 && dateQuery !== ""'));
 		query = ` WHERE ${dateQuery} AND ${sqlQueryParams.join(' AND ')}`;
 	}
 
 	if (req.params.action === 'load-fetch') {
+
 		const sql = 'SELECT * FROM `katek`.`test-500k-limes` LIMIT 1000';
 
-		console.log(sql);
+		console.log(chalk.cyan(sql));
 
 		connection.query(sql, (error, results) => {
 			if (error)
@@ -58,9 +60,12 @@ app.get('/:action', (req: Request, res: Response) => {
 			res.send(results);
 		});
 	} else if (req.params.action === 'db-fetch') {
+
+		console.log(req.query);
+
 		const sql = `SELECT * FROM \`katek\`.\`test-500k-limes\`${query} LIMIT 1000`;
 
-		console.log(sql);
+		console.log(chalk.cyan(sql));
 
 		connection.query(sql, (error, results) => {
 			if (error)
@@ -71,7 +76,7 @@ app.get('/:action', (req: Request, res: Response) => {
 	} else if (req.params.action === 'get-countpass') {
 		const sql = `SELECT \`CountPass\` from \`katek\`.\`test-500k-limes\`${query}`;
 
-		console.log(sql);
+		console.log(chalk.cyan(sql));
 
 		connection.query(sql, (error, results) => {
 			if (error)
